128c81925fd1d61f35b65a924f81182645ed3f75
yezi1 <13719490809@163.com>
2017-12-14 23:30:27 +0800

    新增歌曲URL接口

--- page/foundThatMusic/foundThatMusic.js
+++ page/foundThatMusic/foundThatMusic.js
@@ -10,16 +10,21 @@ Page({
     searchtext: '',
     positionNum: '7',
     inputStatus: false, 
+    musicHash: null,
+poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    name: '此时此刻',
+    author: '许巍',
+    src: 'http://fs.w.kugou.com/201712142213/e77e42385f5006fa399cd1c90bc04094/G059/M02/17/1D/ew0DAFdr9KmAf5GnADSkTnjFCm0437.mp3'    
   },
 
   inputState: function (event) {
     this.data.searchtext = event.detail.value;
     if (this.data.searchtext.length > 0) {
       this.data.searchState = true;
+      this.requestData();
     }else{
       this.data.searchState = false;
     }
-    this.requestData();
     this.setData({
       searchState: this.data.searchState  
     });
@@ -32,9 +37,11 @@ Page({
   //   });
   // },
 
+  //搜索音乐接口
   requestData: function () {
     var that = this;
     var resData = [];
+    var resHash = [];
     wx.request({
       url: 'http://songsearch.kugou.com/song_search_v2?userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filter=0&_=1510210621763&keyword=' + this.data.searchtext + '&page=1&pagesize=20',
       method: 'GET',
@@ -42,18 +49,18 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       dataType: "jsonp",
-      success: function (res) {
-        //for (var i in res.data) {
+      success: (res) => {
         var jsonRes = JSON.parse(res.data);
         for (var i in jsonRes.data.lists) {
-          resData.push(jsonRes.data.lists[i]);
-        }
-        console.log(resData);
-        that.setData({
-          SongName: resData
+          resData.push(jsonRes.data.lists[i].FileName.replace(/<\/?em[^>]*>/gi, ''));
+          resHash.push(jsonRes.data.lists[i].FileHash);
+        };
+        this.setData({
+          songName: resData,
+          songHash: resHash
         });
       },
-      fail: function (err) {
+      fail: (err) => {
         console.log(err.errMsg);
       }
     });    
@@ -95,10 +102,35 @@ Page({
     });
   },
 
+  playMusic: function (event) {
+    this.data.musicHash = event.currentTarget.dataset.hash;
+    var that = this;
+    wx.request({
+      url: 'http://www.kugou.com/yy/index.php?r=play/getdata&hash=' + that.data.musicHash,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' 
+      },
+      dataType: "jsonp",
+      success: (res) => {
+        var jsonRes = JSON.parse(res.data);
+        this.setData({
+          musiPlay: jsonRes.data.play_url
+        });
+      },
+      fail: (err) => {
+        console.log(err.errMsg);
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '当前页面当前页面当前页面当前页面当前页面'
+    })
     let that = this;    
     wx.request({
       url: getapp.localIp() + 'WYmusic/data/music.json',
@@ -167,14 +199,17 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function (e) {
     let res = wx.getSystemInfoSync();
     let imgW1 = res.screenWidth*0.328;
+    this.audioCtx = wx.createAudioContext('myAudio')
     this.setData({
       imgW1: imgW1
     });
   },
-
+  audioPlay: function () {
+    this.audioCtx.play()
+  },
   /**
    * 生命周期函数--监听页面显示
    */
